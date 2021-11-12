86506e801b357d84538e1e87c2be5d6f8e407a4e
Dastan Ozgeldi <ozgdastan@gmail.com>
2021-11-13 00:44:35 +0600

    ✅ Tree structure change [src/ -> root dir]

--- imaginarium/imaginarium/wsgi.py
+++ imaginarium/imaginarium/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.imaginarium.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imaginarium.settings')
 
 application = get_wsgi_application()
