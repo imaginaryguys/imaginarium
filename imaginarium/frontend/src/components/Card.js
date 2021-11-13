315a16d13826785203709aaeaeb76c400b5e7351
EraOfCoding <63988030+EraOfCoding@users.noreply.github.com>
2021-11-13 14:11:31 +0600

    frontend-dev

--- imaginarium/frontend/src/components/Card.js
+++ imaginarium/frontend/src/components/Card.js
@@ -4,9 +4,15 @@ import '../style/Card.css'
 function Card(props) {
     return (
         <div className="card-container">
-            <h5 className="card-heading">{props.headline}</h5>
             <img className="card-img" src={props.img} />
-            <p className="card-date">{props.date}</p>
+            <div className="card-text-container">
+                <h3 className="card-heading">{props.headline}</h3>
+                <div className="card-properties">
+                    <h5>Used filter: {props.filterName}</h5>
+                    <h5>Author: <a className="card-author" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">{props.authorName}</a></h5>
+                </div>
+                <p className="card-date">{props.date}</p>
+            </div>
         </div>
     )
 }
--- imaginarium/frontend/src/pages/Home.js
+++ imaginarium/frontend/src/pages/Home.js
@@ -18,13 +18,21 @@ function Home() {
     }
 
     let date = new Date()
+
     return (
         <div className="home-container">
-            <h1>Top 10 of the day</h1>
+            <h1 className="home-headline">Top 10 of the day</h1>
             <div className="cards">
+                <Card headline="Communistic rabbit" authorName="Yerassyl" filterName="communism" date={dateBuilder(date)} img="https://i.kym-cdn.com/entries/icons/original/000/034/467/Communist_Bugs_Bunny_Banner.jpg" />
+                <Card headline="USA rabbit" authorName="Yerassyl" filterName="USA" date={dateBuilder(date)} img="https://img.ifunny.co/images/603eb75f6c1f85eea144f1aff06ad8e1b88a3da9719cdd078bedb09c9ae1cbee_1.jpg" />
+                <Card headline="JoJo smile" authorName="Yerassyl" filterName="JoJo smile" date={dateBuilder(date)} img="https://cdn.discordapp.com/attachments/744883397868126340/908953335296569354/jojosmile.gif" />
+                <Card headline="-1000 social credit" authorName="Yerassyl" filterName="Bobb Ross" date={dateBuilder(date)} img="https://cdn.discordapp.com/attachments/744883397868126340/908960502128971836/bobross.gif" />
+                <Card headline="USA rabbit" date={dateBuilder(date)} img="https://img.ifunny.co/images/603eb75f6c1f85eea144f1aff06ad8e1b88a3da9719cdd078bedb09c9ae1cbee_1.jpg" />
+                <Card headline="JoJo smile" date={dateBuilder(date)} img="https://cdn.discordapp.com/attachments/744883397868126340/908953335296569354/jojosmile.gif" />
                 <Card headline="Communistic rabbit" date={dateBuilder(date)} img="https://i.kym-cdn.com/entries/icons/original/000/034/467/Communist_Bugs_Bunny_Banner.jpg" />
                 <Card headline="USA rabbit" date={dateBuilder(date)} img="https://img.ifunny.co/images/603eb75f6c1f85eea144f1aff06ad8e1b88a3da9719cdd078bedb09c9ae1cbee_1.jpg" />
-                <Card headline="Communistic chungus" date={dateBuilder(date)} img="https://i.redd.it/uy8hpx8rzl621.jpg" />
+                <Card headline="JoJo smile" date={dateBuilder(date)} img="https://cdn.discordapp.com/attachments/744883397868126340/908953335296569354/jojosmile.gif" />
+                <Card headline="JoJo smile" date={dateBuilder(date)} img="https://cdn.discordapp.com/attachments/744883397868126340/908953335296569354/jojosmile.gif" />
             </div>
         </div>
     );
